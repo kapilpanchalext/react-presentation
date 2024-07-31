e7910f5490acbf0447546c7639776466cf03798c
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-07-31 17:34:31 +0530

    [refactor] css

--- presentation-feature-card-1/src/App.tsx
+++ presentation-feature-card-1/src/App.tsx
@@ -1,10 +1,42 @@
 import './App.css';
+import Slide from './components/slide/Slide';
 
 function App() {
 
   return (
     <>
-      <h1>Presentation</h1>
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+        <h2>Presentation</h2>
+        <Slide />
+        <div style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
+          <button className="btn success">
+            <span className="material-symbols-outlined"
+              style={{
+                      fontSize: "48px", 
+                      borderRadius: "50%", 
+                      backgroundColor: "#f0f0f0", 
+                      padding: "10px",
+                      display: "inline-flex",
+                      alignItems: "center",
+                      justifyContent: "center"
+                    }}>{"arrow_back_2"}
+            </span>
+          </button>
+          <button className="btn success">
+            <span className="material-symbols-outlined"
+              style={{
+                      fontSize: "48px", 
+                      borderRadius: "50%", 
+                      backgroundColor: "#f0f0f0", 
+                      padding: "10px",
+                      display: "inline-flex",
+                      alignItems: "center",
+                      justifyContent: "center"
+                    }}>{"play_arrow"}
+            </span>
+          </button>
+        </div>
+      </div>
     </>
   )
 }
---
+++ presentation-feature-card-1/src/components/slide/Slide.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Slide = () => {
+  return (
+    <div className="card flex flex-container" style={{ width: '1080px', height: '720px' }}>
+      <div className='flex flex-container' style={{ flexDirection: 'column' }}>
+        <div className="container">
+          <h4><b>Presentation 1</b></h4>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Slide;
